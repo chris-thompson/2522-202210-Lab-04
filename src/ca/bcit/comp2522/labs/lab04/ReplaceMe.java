f1746892ab0440dacd9374f35ad224d2f32b7f99
Chris Thompson <chris_thompson@bcit.ca>
2022-01-31 13:13:45 -0800

    Hello world!

---
+++ src/ca/bcit/comp2522/labs/lab04/ReplaceMe.java
@@ -0,0 +1,7 @@
+package ca.bcit.comp2522.labs.lab04;
+
+public class ReplaceMe {
+    public static void main(String[] args) {
+        System.out.println("Replace this file with your work!");
+    }
+}
